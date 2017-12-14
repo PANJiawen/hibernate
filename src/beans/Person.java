9ceb89ee35f452558c7399bf9ba4ea1d28b88bd3
JiawenPAN <pjwadsn2@gmail.com>
2017-12-14 20:10:24 +0100

    </dependency> 	  	<dependency>   		<groupId>mysql</groupId>   		<artifactId>mysql-connector-java</artifactId>   		<version>5.1.38</version>   	</dependency>

---
+++ src/beans/Person.java
@@ -0,0 +1,18 @@
+package beans;
+
+public class Person {
+	private int idh;
+	private String name;
+	public int getIdh() {
+		return idh;
+	}
+	public void setIdh(int idh) {
+		this.idh = idh;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
